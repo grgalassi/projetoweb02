5be2fc4227ff0ac18859d3a19c4f1e97248b4b12
Gustavo <grgalassi@gmail.com>
2022-07-01 12:35:44 -0300

    Inserçao Controller, Swagger e DTO

---
+++ src/main/java/br/com/cotiinformatica/dtos/PostEmpresaDTO.java
@@ -0,0 +1,14 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PostEmpresaDTO {
+	
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String cnpj;
+
+}
